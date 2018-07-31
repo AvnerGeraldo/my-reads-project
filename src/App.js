6a55171a77fca7431aa937fb596be4b2a1607694
Avner <avner.conceicao@hotmail.com>
2018-07-31 23:39:29 +0000

    Add Component SearchPage a App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
 //Components
 import ButtonChooseShelf from './ButtonChooseShelf/ButtonChooseShelf'
 import Shelf from './Shelf/Shelf'
+import SearchPage from './SearchPage/SearchPage'
 
 //Assets
 import './App.css'
@@ -41,38 +42,48 @@ class BooksApp extends React.Component {
   render() {
     const listBookShelf = ['Currently Reading', 'Want to Read', 'Read', 'All']
 
-    return (
-      <div className="container-fluid">         
-        <div className="row align-items-center">
-          <div className="col list-books-title">
-            <h1>MyReads</h1>
-          </div>
-        </div>
-        <div className="row choose-bookshelf">
-          <div className="col-10 offset-1">
-            <div className="row">
-              {listBookShelf.map(shelf => (
-                <div 
-                  key={shelf} 
-                  className="col-3">
-                    <ButtonChooseShelf 
-                      bookshelf={shelf}
-                      changeShelf={this.changeShelfHandler}
-                      active={shelf === this.state.shelfActive}/>
-                </div>
-              ))}
-            </div>          
-          </div>
-        </div>
+    return (   
+      <div className="container-fluid">
         <Route 
           path='/' 
           exact
           render={() => (
-            <Shelf 
-              data={this.state.bookData} 
-              chooseShelf={this.state.shelfActive}
-              changeBookShelf={this.changeBookShelfHandler} />
-          )} />      
+            <div>
+                <div className="row align-items-center">
+                    <div className="col list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                </div>
+                <div className="row choose-bookshelf">
+                    <div className="col-10 offset-1">
+                        <div className="row">
+                            {listBookShelf.map(shelf => (
+                              <div 
+                                key={shelf} 
+                                className="col-3">
+                                  <ButtonChooseShelf 
+                                    bookshelf={shelf}
+                                    changeShelf={this.changeShelfHandler}
+                                    active={shelf === this.state.shelfActive}/>
+                              </div>
+                            ))}
+                        </div>          
+                      </div>
+                      <div className="open-search">
+                          <Link to="/search">Add a Book</Link>
+                      </div>
+                  </div>
+                  <Shelf 
+                    data={this.state.bookData} 
+                    chooseShelf={this.state.shelfActive}
+                    changeBookShelf={this.changeBookShelfHandler} />
+              </div>
+          )} />
+          <Route
+            path = '/search'
+            render = {() => (
+              <SearchPage />
+            )} />   
       </div>
     )
   }
